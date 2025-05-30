ec02f6f9b065a2d522eb4d9b0b0c0acc7eb0240a
Jonathan Tsang <jw.tsang@gmail.com>
2025-05-30 18:03:05 -0300

    style filter dropdown

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -33,12 +33,12 @@ const SearchBar: React.FC<SearchBarProps> = ({
         className="flex-grow px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
         aria-label="Global Search Text"
       />
-      <details>
+      <details className="relative">
         <summary className="mr-2 font-mono text-3xl marker:content-none cursor-pointer">
           &#x2699;
         </summary>
         <fieldset
-          className="flex flex-wrap gap-2 mt-2 sm:mt-0"
+          className="bg-white border border-gray-300 w-[150px] flex flex-col flex-wrap gap-2 mt-2 p-4 sm:mt-0 absolute top-full right-0 z-50"
           aria-label="Searchable columns selection">
           {columns.map((col) => (
             <label
@@ -48,10 +48,10 @@ const SearchBar: React.FC<SearchBarProps> = ({
                 type="checkbox"
                 checked={selectedColumns.includes(col)}
                 onChange={() => toggleColumn(col)}
-                className="form-checkbox h-4 w-4 text-blue-600"
+                className="form-checkbox h-4 w-4 text-blue-600 mr-1"
                 aria-checked={selectedColumns.includes(col)}
               />
-              <span className="text-gray-700 text-sm">{col}</span>
+              <span className="text-gray-700 text-sm text-nowrap">{col}</span>
             </label>
           ))}
         </fieldset>
